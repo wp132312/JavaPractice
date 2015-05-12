de9da8fa546ac2e27ab43225b89996169129371c
ryusim <ryusim@172.17.1.174>
2015-05-12 11:49:30 +0900

    ㅁㅇㄴㄹㅁㅇㄴㄹㄴㅇㅁㄹ

---
+++ JavaPractice/src/org/dimigo/oop/Question.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+import java.util.Scanner;
+
+/**
+ * <pre>
+ * org.dimigo.oop.Question
+ *
+ * 1. Scheme 	:  문제 3문제 내고 맞추기.
+ * 2. Date 		: 2015. 5. 12.
+ * </pre>
+ *
+ * @author		: ryusim
+ * @version		: 1.0 
+ */
+public class Question {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		String answer;
+		
+		System.out.println("가장 좋아하는 음악가는?");
+		answer = scanner.nextLine();
+		if(answer != null && answer.equals("리스트")) {
+			System.out.println("정답입니다!! ^^\n");
+		} else {
+			System.out.println("틀렸습니다!! ㅠ\n");
+		}
+		
+		System.out.println("가장 좋아하는 과목은?");
+		answer = scanner.nextLine();
+		if(answer != null && answer.equals("체육")) {
+			System.out.println("정답입니다!! ^^\n");
+		} else {
+			System.out.println("틀렸습니다!! ㅠ\n");
+		}
+		
+		System.out.println("가장 좋아하는 음악은?");
+		answer = scanner.nextLine();
+		if(answer != null && answer.equals("라 캄파넬라")) {
+			System.out.println("정답입니다!! ^^\n");
+		} else {
+			System.out.println("틀렸습니다!! ㅠ\n");
+		}
+		
+		scanner.close();
+	}
+
+}

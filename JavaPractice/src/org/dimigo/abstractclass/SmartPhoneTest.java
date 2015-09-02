b075d8fd427c626c9b65c42e322e6f52999e4f49
백승윤 <ryusim@thepabix.com>
2015-09-02 12:11:35 +0900

    추상클래스.

---
+++ JavaPractice/src/org/dimigo/abstractclass/SmartPhoneTest.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package org.dimigo.abstractclass;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance.SmartPhoneTest
+ *
+ * 1. Scheme 	: 
+ * 2. Date 		: 2015. 8. 28.
+ * </pre>
+ *
+ * @author		: ryusim
+ * @version		: 1.0 
+ */
+public class SmartPhoneTest {
+	public static void main(String[] args) {
+		SmartPhone i = new IPhone("IPhone 6", "애플", 700000);
+		SmartPhone g = new Galaxy("갤럭시 S6", "삼성", 650000);
+		
+		System.out.println(i.toString());
+		i.turnOn();
+		i.pay();
+		i.useSpecialFunction();
+		i.turnOff();
+		
+		System.out.println();
+		
+		System.out.println(g.toString());
+		g.turnOn();
+		g.pay();
+		g.useSpecialFunction();
+		g.turnOff();
+	}
+}

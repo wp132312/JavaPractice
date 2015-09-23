ade1be73bf50e72187dffb4486ba6fb32667dca7
백승윤 <ryusim@thepabix.com>
2015-09-23 12:44:24 +0900

    asdfsdfdsf

--- JavaPractice/src/org/dimigo/exception/MovieTest.java
+++ JavaPractice/src/org/dimigo/exception/MovieTest.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package org.dimigo.exception;
 
 /**
@@ -18,21 +15,21 @@ public class MovieTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		try {
 			Movie[] movies = {
+					new Movie("베테랑", 15),
 					new Movie("앤트맨", 12), 
-					new Movie("사도", 12), 
-					new Movie("베테랑", 15)
+					new Movie("사도", 12)
 			};
 			
 			int age = 13;
 			
 			for(Movie movie : movies) {
-				buyTicket(movie, age);
+				try {
+					buyTicket(movie, age);
+				} catch(Exception e) {
+					System.out.println(e.getMessage());
+				}
 			}
-		} catch(Exception e) {
-			System.out.println(e.getMessage());
-		}
 	}
 	
 	private static void buyTicket(Movie movie, int age) throws Exception{
